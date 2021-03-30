6a2f8f605f854eff2a9a084aab5b56f8f0db05c6
ckemmerer22 <50115842+ckemmerer22@users.noreply.github.com>
2021-03-29 22:11:31 -0400

    Update notifySMS.py
    Minor tweaks and addition of ability to use rite-aid zip lookup API

--- notifySMS.py
+++ notifySMS.py
@@ -2,27 +2,40 @@
 
 import requests, urllib3
 from twilio.rest import Client
+
+client = Client("<INSERT>", "<INSERT>")
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Add your notification phone here for SMS messages
-notify_phone = "+15555551212"
+zip_codes = ['<INSERT>']
 
-# Add your Twilio number (which will be the sender)
-sender_phone = "+15555551234"
+stores_to_check = []
 
-# Add your own Twilio creds here
-client = Client("BLANK", "BLANK")
+for zip in zip_codes: # I set this to 30mi radius, you can set lower, at least for time being PREF-112 URL query param should be kept to ensure location offers covid vax
+        url = "https://www.riteaid.com/services/ext/v2/stores/getStores?address={0}".format(zip) + "&attrFilter=PREF-112&fetchMechanismVersion=2&radius=30"
+        response = requests.get(url, verify = False)
+        responsej = response.json()
+        for s in range(0,len(responsej['Data']['stores'])):
+                stores_to_check.append(responsej['Data']['stores'][s]['storeNumber'])
+                
+stores_to_check_dd = []
 
-# Add your nearby stores (five digit numbers)
-stores_to_check =   ("12345","09876")
+[stores_to_check_dd.append(x) for x in stores_to_check if x not in stores_to_check_dd] # remove dupes in overlap between zips
 
-for store in stores_to_check:
+# from the stores in our geo range, let's check slot availaibility
+for store in stores_to_check_dd:
     url = "https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber={0}".format(store)
     response = requests.get(url, verify = False)
     responsej = response.json()
     if (responsej["Data"]["slots"]["1"]) or (responsej["Data"]["slots"]["2"]):
-        print("Found a match " + store)
-        message = "Opening found at Rite Aid store number " + store + " book at riteaid.com/pharmacy/covid-qualifier"
-        client.messages.create(to=notify_phone, from_=sender_phone, body=message)
+        s = str(store)
+        print("Found a match " + s)
+        if len(s) == 4: # deal with padding of 0's
+            s = "0" + s
+        if len(s) == 3:
+            s = "00" + s
+        # send a sms telling open availability for appt
+        message = "Opening found at Rite Aid store number " + s + " book at riteaid.com/pharmacy/covid-qualifier; store details at https://www.riteaid.com/locations/search.html?id=" + s
+        client.messages.create(to="<INSERT>", from_="<INSERT>", body=message)
     else:
-        print("No opening at store " + store)
+        print("No opening at store " + str(store))
